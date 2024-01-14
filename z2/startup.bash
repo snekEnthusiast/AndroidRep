4fddd8789ba2a70129a1aac2701cd1efd3c76a88
Mikolaj <mikolaj4jach@gmail.com>
2024-01-14 15:23:14 +0100

    m

--- z2/startup.bash
+++ z2/startup.bash
@@ -1,2 +1,1 @@
-#(cd ktor-sample/build/classeskotlin/main;java com.example.ApplicationKt) & (sleep 20; ngrok http http://127.0.0.1:8080)
-(cd ktor-sample/build/classeskotlin/main;java com.example.ApplicationKt)
+(cd ktor-sample/build/classeskotlin/main;java com.example.ApplicationKt) & (sleep 50; ngrok http http://127.0.0.1:8080)
