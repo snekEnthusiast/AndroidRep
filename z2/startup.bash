935f677db436ad8d803e4e7f04efb77762b6a580
Mikolaj <mikolaj4jach@gmail.com>
2024-01-18 12:39:33 +0100

    typo

--- z2/startup.bash
+++ z2/startup.bash
@@ -1,2 +1,2 @@
-grok config add-authtoken 2awTiwetasaTNbi83OswgYYxG5A_3XSRC3rAckdZcUPh1eiNk
+ngrok config add-authtoken 2awTiwetasaTNbi83OswgYYxG5A_3XSRC3rAckdZcUPh1eiNk
 (cd ktor-sample/build/classeskotlin/main;java com.example.ApplicationKt) & (sleep 50; ngrok http http://127.0.0.1:8080)
